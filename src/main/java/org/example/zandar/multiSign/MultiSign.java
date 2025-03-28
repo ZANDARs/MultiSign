9d7d37351e02db76ba831fe834f2c6a84e2bef38
zandar <jokez.tik.bs@gmail.com>
2025-03-28 19:40:40 +0200

    added signature limit, ability to change limit via config

--- src/main/java/org/example/zandar/multiSign/MultiSign.java
+++ src/main/java/org/example/zandar/multiSign/MultiSign.java
@@ -8,10 +8,13 @@ import java.lang.reflect.Field;
 import java.util.Objects;
 
 public final class MultiSign extends JavaPlugin {
+    public int maxSignatures;
 
     @Override
     public void onEnable() {
         registerCommands();
+        maxSignatures = getConfig().getInt("max-signatures", 2);
+        saveDefaultConfig();
 
         // Plugin startup logic
 
@@ -29,7 +32,7 @@ public final class MultiSign extends JavaPlugin {
             commandMapField.setAccessible(true);
             CommandMap commandMap = (CommandMap) commandMapField.get(getServer());
 
-            SignCommand signCommand = new SignCommand();
+            SignCommand signCommand = new SignCommand(this);
             commandMap.register("signbook", signCommand);
         } catch (Exception e) {
             getLogger().severe("Не вдалося зареєструвати команду!");
--- src/main/java/org/example/zandar/multiSign/commands/SignCommand.java
+++ src/main/java/org/example/zandar/multiSign/commands/SignCommand.java
@@ -8,14 +8,18 @@ import org.bukkit.command.defaults.BukkitCommand;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.BookMeta;
+import org.example.zandar.multiSign.MultiSign;
 import org.jetbrains.annotations.NotNull;
 
 import java.util.Collections;
 
 public class SignCommand extends BukkitCommand {
 
-    public SignCommand() {
+    private final MultiSign plugin;
+
+    public SignCommand(MultiSign plugin) {
         super("signbook");
+        this.plugin = plugin;
         this.description = "Додає підпис до книги";
         this.usageMessage = "/signbook";
         this.setAliases(Collections.singletonList("sign"));
@@ -33,6 +37,14 @@ public class SignCommand extends BukkitCommand {
                     player.sendMessage("Ви вже підписали цю книгу!");
                     return true;
                 }
+                int currentSignatures = oldAuthor.isEmpty() ? 0 : oldAuthor.split(" & ").length;
+
+
+                if (currentSignatures >= plugin.maxSignatures) {
+                    player.sendMessage("Досягнуто максимальну кількість підписантів (" + plugin.maxSignatures + ")");
+                    return true;
+                }
+
                 meta.setAuthor(oldAuthor + " & " + player.getName());
                 item.setItemMeta(meta);
 
